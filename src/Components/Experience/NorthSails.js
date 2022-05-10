2d7fa70d25c218b12846ad1cae4be3c195c97547
IllusiveBagel <hawkentroop@gmail.com>
2022-05-10 22:37:30 +0100

    Added Content to North Sails Page

--- src/Components/Experience/NorthSails.js
+++ src/Components/Experience/NorthSails.js
@@ -7,15 +7,66 @@ export default class NorthSails extends React.Component {
             <div>
                 <h1 className="Content-Title">North Sails</h1>
                 <p className="Content-Text">
-                    To Be Written
+                    My role at North Sails was as a Full Stack developer helping build and
+                    maintain an internal sales and stock management system. This system was
+                    built with a C# and .NET Core REST API and a ReactJS Front End using
+                    MaterialUI. When I joined the company the REST API was built in VB so
+                    a lot of my time was spent converting this to the more modern .NET Core
+                    version. I was also given the task of converting a lot of old pages from
+                    a large mix of technologies to use React and create a few new pages to
+                    expand functionality.
                     <br /><br />
+                    With the North Sails development team being made up of myself and 1 other
+                    person there was lots of oppurtunity for me to play a part in the design
+                    of the software as well as the actual development of it so while at North
+                    Sails I helped plan and prototype interesting projects such as a mobile
+                    app for scanning QR codes to help stock management in the production
+                    process of the sails.
+                    <br /><br />
+                    At North Sails I picked up some valuable skills such as the ability to
+                    refactor other peoples code in a logical and thought out way to ensure
+                    all bases are covered. North Sails was also my first role where I could
+                    apply my Typescript knowledge so I was able to take what I knew and use
+                    it in a professional environment. While at North Sails I also became
+                    more proficient at displaying large amounts of data in a user friendly
+                    way.
                 </p>
                 <h1 className="Content-SubTitle">Skills Used</h1>
                 <div className="Content-Skills">
                     <div className="Content-Skill">
-                        <p>To Be Written</p>
+                        <p>React</p>
                         <div className="Content-Skill-Container">
                             <div className="Content-Skill-Status" style={{ width: "80%" }}></div>
+                        </div> 
+                    </div>
+                    <div className="Content-Skill">
+                        <p>C#</p>
+                        <div className="Content-Skill-Container">
+                            <div className="Content-Skill-Status" style={{ width: "40%" }}></div>
+                        </div> 
+                    </div>
+                    <div className="Content-Skill">
+                        <p>Visual Basic</p>
+                        <div className="Content-Skill-Container">
+                            <div className="Content-Skill-Status" style={{ width: "15%" }}></div>
+                        </div> 
+                    </div>
+                    <div className="Content-Skill">
+                        <p>REST API's</p>
+                        <div className="Content-Skill-Container">
+                            <div className="Content-Skill-Status" style={{ width: "60%" }}></div>
+                        </div> 
+                    </div>
+                    <div className="Content-Skill">
+                        <p>MaterialUI</p>
+                        <div className="Content-Skill-Container">
+                            <div className="Content-Skill-Status" style={{ width: "80%" }}></div>
+                        </div> 
+                    </div>
+                    <div className="Content-Skill">
+                        <p>TSQL</p>
+                        <div className="Content-Skill-Container">
+                            <div className="Content-Skill-Status" style={{ width: "40%" }}></div>
                         </div> 
                     </div>
                 </div>
