7e8e27da109877e6bea9898f55fbd3896390c71b
logan.young <logan.young@autoprotectgroup.co.uk>
2023-02-12 15:25:28 +0000

    Color reskin

--- src/Pages/Blog/Blog.jsx
+++ src/Pages/Blog/Blog.jsx
@@ -4,7 +4,7 @@ import styles from './Blog.module.scss';
 
 const Blog = () => {
     return (
-        <div className={styles.blogContainer}>Blog</div>
+        <div className={styles.blogContainer}><h1 style={{ color: 'white' }}>Blog</h1></div>
     );
 }
 
