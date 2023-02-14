2f8491e1106742ecf7fe599a0bc794817db7811f
logan.young <logan.young@autoprotectgroup.co.uk>
2023-02-14 11:34:43 +0000

    contact page redesign

--- src/Pages/Contact/Contact.jsx
+++ src/Pages/Contact/Contact.jsx
@@ -1,32 +1,26 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons'
+import { faLinkedinIn, faGithub } from '@fortawesome/free-brands-svg-icons';
 
 import styles from './Contact.module.scss';
 
 const Contact = () => {
     return (
         <div className={styles.contactContainer}>
-            <div className={styles.contactLeft}>
-                <img className={styles.contactIcon} src="images/envelope.svg" alt="Envelope" />
-            </div>
             <div className={styles.contactBlock}>
-                <h1 className={styles.contactTitle}>Get In Touch</h1>
+                <h1>Get In Touch!</h1>
+                <h3>I'm open to any inquiries and more than happy to talk about my projects</h3>
                 <div className={styles.contactLinks}>
-                    <div className={styles.contactLink}>
-                        <p>Phone</p>
-                        <a href="tel:07446696964">07446696964</a>
-                    </div>
-                    <div className={styles.contactLink}>
-                        <p>Email</p>
-                        <a href="mailto:logan@neutral.systems">logan@neutral.systems</a>
-                    </div>
-                    <div className={styles.contactLink}>
-                        <p>LinkedIn</p>
-                        <a href="https://www.linkedin.com/in/lky/" target="_blank" rel="noreferrer">linkedin.com/in/lky</a>
-                    </div>
-                    <div className={styles.contactLink}>
-                        <p>Github</p>
-                        <a href="https://github.com/IllusiveBagel" target="_blank" rel="noreferrer">github.com/IllusiveBagel</a>
-                    </div>
+                    <a href="mailto:logan@neutral.systems">
+                        <FontAwesomeIcon icon={faEnvelope} className={styles.contactIcon} size="3x" />
+                    </a>
+                    <a href="https://www.linkedin.com/in/lky/">
+                        <FontAwesomeIcon icon={faLinkedinIn} className={styles.contactIcon} size="3x" />
+                    </a>
+                    <a href="https://github.com/IllusiveBagel">
+                        <FontAwesomeIcon icon={faGithub} className={styles.contactIcon} size="3x" />
+                    </a>
                 </div>
             </div>
         </div>
