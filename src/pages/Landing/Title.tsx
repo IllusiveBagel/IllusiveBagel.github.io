5c76f056f6abbbe203c6efb47b9d4029e390d884
logan.young <logan.young@autoprotectgroup.co.uk>
2024-07-19 12:32:43 +0100

    Updated Landing Title

--- src/pages/Landing/Title.tsx
+++ src/pages/Landing/Title.tsx
@@ -1,17 +1,19 @@
-import styles from "./Landing.module.scss";
+import styles from './Landing.module.scss';
 
 const Title = () => {
-    return(
+    return (
         <div className={styles.title}>
             <h1>Logan Young</h1>
             <div className={styles.subTitle}>
-                <h4>React Developer</h4>
-                <h4 style={{ color: "#93a1a1" }}>and</h4>
+                <h4>Web Developer</h4>
+                <h4 style={{ color: '#93a1a1' }}>and</h4>
                 <h4>Maker</h4>
             </div>
-            <a className={styles.learn} href="#About">Learn More</a>
+            <a className={styles.learn} href="#About">
+                Learn More
+            </a>
         </div>
     );
-}
+};
 
 export default Title;
